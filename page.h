b65bd408eb3f5c28eb34a25453cf2e571347b3f7
Gab-Mel <gabrielzinho.melo.10@gmail.com>
2023-11-18 08:19:05 -0300

    setando pagina

---
+++ page.h
@@ -0,0 +1,14 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Page {
+    string keys; // chave
+    Page **pais; // Paginas pais
+    int n; // Número de páginas filhos
+    flout probabilidade; //probabilidade da página
+}
